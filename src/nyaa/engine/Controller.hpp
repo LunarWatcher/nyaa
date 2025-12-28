4955171fa175cc94b0f530420d7988da063304a2
Olivia <oliviawolfie@pm.me>
2025-12-28 14:46:41 +0100

    Fix clangd checks

--- src/nyaa/engine/Controller.hpp
+++ src/nyaa/engine/Controller.hpp
@@ -4,6 +4,8 @@ namespace nyaa::engine {
 
 class Controller {
 public:
+    virtual ~Controller() = default;
+
     virtual void render(double delta) = 0;
     virtual void tick(double delta) = 0;
 };
